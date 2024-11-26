7f4e718e1b676c495d6d1829a20cebad6be26145
Trofimov Dmitry <winprint63@gmail.com>
2024-11-26 10:44:33 +0400

    new class in HW17, method isEqual compareTo

--- src/Homework/HW17/Substr1.java
+++ src/Homework/HW17/Substr1.java
@@ -1,4 +1,14 @@
 package Homework.HW17;
 
 public class Substr1 {
+    public void isEqual (StringBuilder sb1, StringBuilder sb2){
+
+        System.out.println(sb1==sb2);
+        boolean areEqual = sb1.toString().compareTo(sb2.toString()) == 0;
+        System.out.println(areEqual);
+    }
+    public static void main(String[] args) {
+        Substr1 tester = new Substr1();
+        tester.isEqual(new StringBuilder("qwer"), new StringBuilder("qwer"));
+    }
 }
