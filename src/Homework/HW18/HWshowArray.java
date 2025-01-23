14a2f36d0d19de683160d9b631c644618a9652ee
Trofimov Dmitry <winprint63@gmail.com>
2025-01-23 20:16:51 +0400

    lesson18 hw tests

--- src/Homework/HW18/HWshowArray.java
+++ src/Homework/HW18/HWshowArray.java
@@ -5,10 +5,37 @@ public class HWshowArray {
 
     public static void showArray (String [][] array100){
         System.out.println(Arrays.deepToString(array100));
+        String s = Arrays.toString(array100);
+        System.out.println(s);
+    }
+
+    public static void showArny(String[][] array) {
+        System.out.print("{ ");
+        for (int i = 0; i < array.length; i++){
+            System.out.print("{");
+            for (int j = 0; j < array[i].length; j++) {
+                if (j != array[i].length - 1) {
+                    System.out.print(array[i][j] + ", ");
+                } else {
+                    System.out.print(array[i][j]);
+                }
+            }
+            if (i != array.length -1) {
+                System.out.print("}, ");
+            } else {
+                System.out.print("}");
+            }
+        }
+        System.out.println(" }");
     }
 
     public static void main(String[] args) {
         String [][] ary = {{"odin", "dva"}, {"tri"}, {"4etire", "pyat"}};
+        String [][] array2 = {{"apple", "orange"}, {"hello", "bye", "ok"}, {"car"}};
 showArray(ary);
+showArny(ary);
+showArny(array2);
+
+showArray(array2);
     }
 }
--- src/Homework/HW18/HWsortirovka.java
+++ src/Homework/HW18/HWsortirovka.java
@@ -15,12 +15,41 @@ public class HWsortirovka {
             System.out.println();
 //        }
 
+    }
 
+    public static int [] sortik(int [] array){
+        int a;
+        for (int i =0; i < array.length; i++){
+            int min = array[i];
+            int index = i;
+
+            for (int j = i + 1; j < array.length; j++){
+                if (array[j] < min){
+                    min = array[j];
+                    index = j;
+                    }
+            }
+
+            if (i != index) {
+                a = array[i];
+                array[i] = min;
+                array[index] = a;
+            }
+        }
+        System.out.println();
+        return array;
     }
 
     public static void main(String[] args) {
         int [] array1 = {55, 81, 574, 214, -3, 0, 63};
         sortirovka(array1);
+
+        int [] array2 = {5,8,1,-3,0,8,2,2};
+        sortik(array2);
+        for (int i = 0; i < array2.length; i++ ){
+            System.out.println(array2[i]);
+        }
+
     }
 
 }
